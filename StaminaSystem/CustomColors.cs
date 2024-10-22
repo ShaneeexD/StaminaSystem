94c03f9a0a80281d5df7f94c65fc257c0adf263a
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2024-10-22 09:30:34 +0100

    Minor changes

--- StaminaSystem/CustomColors.cs
+++ StaminaSystem/CustomColors.cs
@@ -2,7 +2,6 @@
 
 public static class CustomColors
 {
-    public static Color Magenta => new Color(1f, 0f, 1f);
     public static Color Transparent => new Color(1f, 1f, 1f, 0f);
     public static Color Red => new Color(1f, 0f, 0f, 1f);
     public static Color Green => new Color(0f, 1f, 0f, 1f);
